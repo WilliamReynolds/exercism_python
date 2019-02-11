c2eef8d5b854946a7a634369ee81ac7f2a355901
WilliamReynolds <wtr8@pitt.edu>
2019-02-11 02:27:18 -0500

    added new exercise acronym

---
+++ acronym/acronym.py
@@ -0,0 +1,12 @@
+phrase = raw_input("What phrase would you like to shorten?\n")
+
+
+def abbreviate(phrase):
+    if phrase:
+        print(phrase)
+    else:
+        print("No phrase entered")
+
+
+abbreviate(phrase)
+
